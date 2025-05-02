e233447f00fdef9d52312845f49b827ab45c371d
Zewen Shang <zewen_shang@mymail.sutd.edu.sg>
2025-05-02 15:06:53 +0800

    tiny update

--- emulator/src/udpsocket.rs
+++ emulator/src/udpsocket.rs
@@ -1,14 +1,14 @@
 use std::net::UdpSocket;
 
-pub fn udp_socket(message: &str) -> std::io::Result<()> {
+pub fn udp_socket(message: &str, port: u16) -> std::io::Result<()> {
     // Bind the socket to a local address
     let socket = UdpSocket::bind("127.0.0.1:8888")?;
 
     // Set the destination address (you can change the port)
-    let target_addr = "127.0.0.1:6666";
+    let target_addr = format!("127.0.0.1:{}",port);
 
     // Send the message
-    socket.send_to(message.as_bytes(), target_addr)?;
+    socket.send_to(message.as_bytes(), &target_addr)?;
 
     println!("Sent message: {}", message);
     Ok(())
