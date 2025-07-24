a7c4513ed863748aa9cb16477fb984c79dd65dc8
Zewen Shang <zewen_shang@mymail.sutd.edu.sg>
2025-07-24 15:25:01 +0800

    Update auto_testing script

--- scripts/exithook_auto_testing_fuzzer.py
+++ scripts/exithook_auto_testing_fuzzer.py
@@ -1,59 +1,79 @@
 import subprocess
 import threading
 import os
-container_name = "strange_ellis"
-exploit_name = "crash6_excep5_pc_2676e"
-# input_file1 = "./target-zephyr/inputs/sm_pairing_rsp_good350.bin"
-cmd = "cd ./U-Fuzz && sudo bin/ble_realtime_fuzzer --exploit="+exploit_name
+import socket
+import time
+
 # Kill the previous running fuzzer
 def stream_output(process, name):
     for line in process.stdout:
         print(f"[{name}] {line}", end='')  # tag output by source
     process.stdout.close()
-# Run the docker exec command and stream output
 
-while True:
-    # Kill the running process if it is not closed properly
-    os.system("docker exec -i "+ container_name +" sh -c 'sudo pkill main_thread'")
-    process1 = subprocess.Popen(
-        ["docker", "exec", "-i", container_name, "sh", "-c", cmd],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        text=True,  # Python 3: auto-decode output to str
-        bufsize=1   # Line-buffered
-    )
-    # os.system("./run-input.sh "+ input_file1)
+def udp_listener(host='0.0.0.0', port=12000):
+    global data_received
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((host, port))
+    # print(f"[UDP] Listening on {host}:{port}")
+    while True:
+        data, addr = sock.recvfrom(1024)
+        data_received = data.decode()
+        print(f"[UDP] Received from {addr}: {data.decode()}")
 
-    # # Print each line as it's received
-    for line in process1.stdout:
-        print(line, end='')  # already includes newline
-        if "Crash detected" in line:
-            print("Kill the process!!")
-            process1.kill()
-    process1.wait()
 
-# Run the docker exec command and stream output
-# process2 = subprocess.Popen(
-#     ["./run-input.sh", input_file1],
-#     stdout=subprocess.PIPE,
-#     stderr=subprocess.STDOUT,
-#     text=True,  # Python 3: auto-decode output to str
-#     bufsize=1   # Line-buffered
-# )
-# Create threads to monitor both
-# t1 = threading.Thread(target=stream_output, args=(process1, "Fuzzer"))
-# t2 = threading.Thread(target=stream_output, args=(process2, "Runner"))
+def get_files_in_container(container, path):
+    cmd = f"docker exec {container} sh -c 'find {path} -maxdepth 1 -name *.cpp'"
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    if result.returncode != 0:
+        print(f"Error: {result.stderr}")
+        return []
+    path_file_list = [line.strip() for line in result.stdout.strip().split('\n') if line.strip()]
+    file_list = []
+    for ele in path_file_list:
+        file_list.append((ele.split("/")[-1]).split(".")[0])
+    print(file_list)
+    return file_list
+if __name__ == "__main__":
+    counter_exploit = 0
+    container_name = "strange_ellis"
+    input_files = get_files_in_container(container_name, "/home/user/U-Fuzz/modules/exploits/ble")
+    print(input_files)
+    # input_file_path = sys.argv[1]
+    # input_files = [f for f in listdir(input_file_path) if isfile(join(input_file_path, f))]
+    exploit_name = input_files[counter_exploit]
+    # input_file1 = "./target-zephyr/inputs/sm_pairing_rsp_good350.bin"
+    cmd = "cd ./U-Fuzz && sudo bin/ble_realtime_fuzzer --exploit="+exploit_name
+    data_received = ""
+    thread = threading.Thread(target=udp_listener, daemon=True)
+    thread.start()
+    # Run the docker exec command and stream output
+    while True:
+        # Kill the running process if it is not closed properly
+        print(data_received)
+        if data_received != "":
+            print(f"exploit needs to be updated")
+            counter_exploit +=1
+            exploit_name = input_files[counter_exploit]
+            cmd = "cd ./U-Fuzz && sudo bin/ble_realtime_fuzzer --exploit="+exploit_name
+            print(exploit_name)
+            data_received = ""
+        print("This is the exploit name"+exploit_name)
+        os.system("docker exec -i "+ container_name +" sh -c 'sudo pkill main_thread'")
+        process1 = subprocess.Popen(
+            ["docker", "exec", "-i", container_name, "sh", "-c", cmd],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,  # Python 3: auto-decode output to str
+            bufsize=1   # Line-buffered
+        )
+        # os.system("./run-input.sh "+ input_file1)
 
-# Start threads
-# t1.start()
-# t2.start()
-
-# # Wait for both to finish
-# t1.join()
-# t2.join()
-
-# # Optionally get return codes
-# print(f"Fuzzer exited with {process1.wait()}")
-# print(f"Runner exited with {process2.wait()}")
-# os.system("docker exec -i "+ container_name +" sh -c 'sudo lsof -i:9000'")
-# print("\nExited with code:", process1.returncode)
+        # # Print each line as it's received
+        for line in process1.stdout:
+            # print(line, end='')  # already includes newline
+            if "Crash detected" in line:
+                # data, addr = server_socket.recvfrom(1024)
+                print("Kill the process!!")
+                time.sleep(2)
+                process1.kill()
+        process1.wait()
--- scripts/exithook_auto_testing_input.py
+++ scripts/exithook_auto_testing_input.py
@@ -5,46 +5,59 @@ from os import listdir
 from os.path import isfile, join
 import time
 from colorama import init, Fore, Style
-input_file_path = "./target-zephyr/meaningful_input_multi_version/v350/"
-# get the list of files in the target directory
-input_files = [f for f in listdir(input_file_path) if isfile(join(input_file_path, f))]
-print(input_files)
-crash_log = []
+import sys
+import socket
+
 
 def verify_log(log_list):
     # set wont contains any duplicate element
     return len(log_list) != len(set(log_list))
-# Run the docker exec command and stream output
-for f in input_files:
-    print(f"{Fore.RED}Testing {f} {Style.RESET_ALL}")
-    process1 = subprocess.Popen(
-        ["./run-input.sh",input_file_path+f],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        text=True,  # Python 3: auto-decode output to str
-        bufsize=1   # Line-buffered
-    )
 
-    # log Crash message
-    for line in process1.stdout:
-        if "Result: Crash" in line or "Result: ExitHook" in line:
-            # process the string with the target parten
-            match_crash = re.search(r'(Crash\s*\{[^}]+\})', line)
-            match_exithooh = re.search(r'(ExitHook\s*\{[^}]+\})',line)
-            if match_crash:
-                crash_info = match_crash.group(1)
-                print(crash_info)
-                crash_log.append(crash_info)
-            elif match_exithooh:
-                crash_info = match_exithooh.group(1)
-                print(crash_info)
-                crash_log.append(crash_info)
-            else:
-                print("No crash info found.")
-            # print(line, end='')  # already includes newline
-    process1.wait()
-    # wait for the u-fuzz engine to restart 
-    time.sleep(3)
-# print(crash_log)
-result = verify_log(crash_log)
-print(f"{Fore.GREEN}Potential Crash : {result} {Style.RESET_ALL}")
+def runner(input_file_path, input_files):
+    crash_log = []
+    for f in input_files:
+        print(f"{Fore.RED}Testing {f} {Style.RESET_ALL}")
+        process1 = subprocess.Popen(
+            ["./run-input.sh",input_file_path+f],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,  # Python 3: auto-decode output to str
+            bufsize=1   # Line-buffered
+        )
+
+        # log Crash message
+        for line in process1.stdout:
+            if "Result: Crash" in line or "Result: ExitHook" in line:
+                # process the string with the target parten
+                match_crash = re.search(r'(Crash\s*\{[^}]+\})', line)
+                match_exithooh = re.search(r'(ExitHook\s*\{[^}]+\})',line)
+                if match_crash:
+                    crash_info = match_crash.group(1)
+                    print(crash_info)
+                    crash_log.append(crash_info)
+                elif match_exithooh:
+                    crash_info = match_exithooh.group(1)
+                    print(crash_info)
+                    crash_log.append(crash_info)
+                else:
+                    print("No crash info found.")
+                # print(line, end='')  # already includes newline
+        process1.wait()
+        # wait for the u-fuzz engine to restart 
+        time.sleep(1)
+    return crash_log
+
+if __name__ == "__main__":
+    # input_file_path = "./target-zephyr/meaningful_input_multi_version/v350/"
+    ip_addr = "127.0.0.1"
+    udp_port = 12000
+    input_file_path = sys.argv[1]
+    # get the list of files in the target directory
+    input_files = [f for f in listdir(input_file_path) if isfile(join(input_file_path, f))]
+    print(input_files)
+    crash_log = runner(input_file_path, input_files)
+    result = verify_log(crash_log)
+    print(f"{Fore.GREEN}Potential Crash : {result} {Style.RESET_ALL}")
+    # Update the server side to change exploit
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.sendto(b"hello",(ip_addr,udp_port))
